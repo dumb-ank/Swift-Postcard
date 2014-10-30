8730d32291fb09b8157303f812f054328cd31f1e
Ankur Bose <ankurbose@Ankur-Boses-iMac-2.local>
2014-10-30 19:00:40 +0530

    Initial Commit

---
+++ SW_PostCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SW_PostCard
+//
+//  Created by Ankur Bose on 30/10/14.
+//  Copyright (c) 2014 Ankur Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
